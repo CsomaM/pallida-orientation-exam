7d5732737e9741426f0c8277d5ed924eb92c64c1
Márton Csoma <csoma.m@gmail.com>
2017-11-17 11:05:32 +0100

    search works, along with partial search

--- orientationexam/src/main/java/com/greenfox/csomam/orientationexam/controllers/MainController.java
+++ orientationexam/src/main/java/com/greenfox/csomam/orientationexam/controllers/MainController.java
@@ -1,10 +1,13 @@
 package com.greenfox.csomam.orientationexam.controllers;
 
+import com.greenfox.csomam.orientationexam.modules.LicencePlate;
 import com.greenfox.csomam.orientationexam.repositories.LicencePlateRepo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
+
+import javax.jws.WebParam;
 
 @Controller
 public class MainController {
@@ -14,7 +17,16 @@ public class MainController {
 
     @RequestMapping({"", "/", "/index"})
     public String mainPage(Model model) {
+        model.addAttribute("searchedplatenumber", new LicencePlate());
         model.addAttribute("platenumbers", licencePlateRepo.findAll());
         return "index";
     }
+
+    @PostMapping("/search")
+    public String add (@ModelAttribute LicencePlate licencePlate, Model model){
+        model.addAttribute("searchedplatenumber", new LicencePlate());
+        model.addAttribute("platenumbers", licencePlateRepo.findLicencePlate(licencePlate.getPlate()));
+        return "index";
+    }
+
 }
--- orientationexam/src/main/java/com/greenfox/csomam/orientationexam/repositories/LicencePlateRepo.java
+++ orientationexam/src/main/java/com/greenfox/csomam/orientationexam/repositories/LicencePlateRepo.java
@@ -1,7 +1,13 @@
 package com.greenfox.csomam.orientationexam.repositories;
 
 import com.greenfox.csomam.orientationexam.modules.LicencePlate;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
+
 public interface LicencePlateRepo extends CrudRepository<LicencePlate, Long> {
+
+    @Query(value = "SELECT * FROM licence_plates WHERE plate LIKE %?1%", nativeQuery = true)
+    List<LicencePlate> findLicencePlate (String plateNum);
 }
